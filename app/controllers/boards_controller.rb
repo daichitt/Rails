8616ccd7c5fd0d74bd3eba012211d73d70975eb1
EC2 Default User <ec2-user@ip-172-31-32-148.ap-northeast-1.compute.internal>
2021-02-28 09:31:17 +0000

    first commit

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,18 +2,23 @@ class BoardsController < ApplicationController
   
   before_action :set_target_board, only: %i[show edit update destroy]
   
-  
+  # 一覧画面
   def index
     @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
     @boards = @boards.page(params[:page])
   end
   
+  # 新規作成画面
   def new
     @board = Board.new(flash[:board])
+    
   end
   
+  # 新規作成画面
   def create
+    
     board = Board.new(board_params)
+    
     if board.save
       flash[:notice] = "「#{board.title}」の掲示板を作成しました"
       redirect_to board
@@ -25,14 +30,17 @@ class BoardsController < ApplicationController
     end
   end
   
+  # 新規作成画面
   def show
     @comment = Comment.new(board_id: @board.id)
   end
   
+  # 新規作成画面
   def edit
      @board.attributes = flash[:board] if flash[:board]
   end
   
+  # 新規作成画面
   def update
     if @board.update(board_params)
       redirect_to @board
@@ -44,17 +52,19 @@ class BoardsController < ApplicationController
     end
   end
   
+  # 新規作成画面
   def destroy
     @board.destroy
     redirect_to boards_path, flash: { notice: "「#{@board.title}」の掲示板が削除されました。"}
   end
   
   private
-  
+  # ストロングパラメーターズ
   def board_params
     params.require(:board).permit(:name, :title, :body, tag_ids: [])
   end
   
+  # 新規作成画面
   def set_target_board
     @board = Board.find(params[:id])
   end
